9cebf506dc8b6232ecf0a2d6116d39fe4855f4cf
Uaena <Haipn72@gmail.com>
2020-10-24 21:19:00 +0700

    first lession service

---
+++ aspnet-core/src/Quizlet_Fake.Application/Lessions/LessionAppService.cs
@@ -0,0 +1,92 @@
+﻿
+using Quizlet_Fake.Courses;
+using Quizlet_Fake.Lessons;
+using Quizlet_Fake.Lesssion;
+using System;
+using System.Collections.Generic;
+
+using System.Threading.Tasks;
+using Volo.Abp.Users;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using System.Linq;
+
+namespace Quizlet_Fake.Lessions
+{
+    public class LessionAppService :
+         CrudAppService<
+             Lesson,//Defines CRUD methods
+             LessionDto, //Used to show 
+             Guid, //Primary key of the  entity
+             PagedAndSortedResultRequestDto, //Used for paging/sorting
+             LessionCreateorUpdateDto>, //Used to create/update 
+         ILessionAppService, ITransientDependency
+    {
+        //public IAbpSession AbpSession { get; set; }
+        public LessionAppService(IRepository<Lesson, Guid> repository, ICurrentUser currentUser, IRepository<Course, Guid> repo) : base(repository)
+        {
+            this._currentUser = currentUser;
+            this._repository = repository;
+            this.courseRepo = repo;
+        }
+        private readonly ICurrentUser _currentUser;
+        private readonly IRepository<Lesson, Guid> _repository;
+        private readonly IRepository<Course, Guid> courseRepo;
+        public override Task<LessionDto> CreateAsync(LessionCreateorUpdateDto input)
+        {
+            var course = courseRepo.FirstOrDefault(x => x.Id == input.CourseId);
+            if(course.UserId == (Guid) _currentUser.Id)
+            {
+                return base.CreateAsync(input);
+            }
+            //input.UserId = AbpSession.UserId;
+            return base.CreateAsync(new LessionCreateorUpdateDto());
+        }
+
+        public List<Lesson> GetLessionOfCourses(Guid id)
+        {
+            var lession = new List<Lesson>();
+            Guid currentId = (Guid)_currentUser.Id;
+            var course = courseRepo.Where(x => x.Id == id).FirstOrDefault();
+            if(currentId == course.UserId)
+            {
+                lession = _repository.Where(x => x.CourseId == id).ToList();
+                return lession;
+            }
+
+
+            return lession;
+        }
+
+        public Task DeleteAsyncc(Guid id)
+        {
+            
+            var lesson = _repository.FirstOrDefault(x => x.Id == id);
+            var course = courseRepo.FirstOrDefault(x => x.Id == lesson.CourseId);
+            if (course.UserId == _currentUser.Id)
+            {
+                return base.DeleteAsync(id);
+            }
+            return base.DeleteAsync(new Guid());
+
+        }
+        public override Task<LessionDto> UpdateAsync(Guid id, LessionCreateorUpdateDto input)
+        {
+
+
+            var lesson = _repository.FirstOrDefault(x => x.Id == id);
+            var course = courseRepo.FirstOrDefault(x => x.Id == lesson.CourseId);
+            if (course.UserId == _currentUser.Id)
+            {
+                return base.UpdateAsync(id, input);
+            }
+            return base.UpdateAsync(new Guid(), input);
+        }
+
+
+
+
+    }
+}
