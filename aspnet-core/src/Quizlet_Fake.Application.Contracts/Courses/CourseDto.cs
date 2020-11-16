9342a364943142877eb65cd6efec7691c32930a3
Uaena <Haipn72@gmail.com>
2020-11-16 23:26:48 +0700

    fix

--- aspnet-core/src/Quizlet_Fake.Application.Contracts/Courses/CourseDto.cs
+++ aspnet-core/src/Quizlet_Fake.Application.Contracts/Courses/CourseDto.cs
@@ -12,11 +12,8 @@ namespace Quizlet_Fake.Courses
         public Guid UserId { get; private set; }
 
         public string AuthorName { set; get; }
-
-        public Guid UserId { get;  set; }
         public DateTime PublishDate { get; set; }
         public int LessonNumber { get; set; }
         public float Price { get; set; }
-        public int wordnumber { get; set; }
     }
 }
